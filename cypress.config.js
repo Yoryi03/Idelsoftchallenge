f096ef9c9d6053742a20fcbc93600701691b0862
Yorgi Rafael Valdez <yoryi.valdez03@gmail.com>
2024-12-17 18:45:03 -0400

    githubaction

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,25 @@
+const { allureCypress } = require('allure-cypress/reporter');
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    retries: {
+      // Configure retry attempts for `cypress run`
+      // Default is 0
+      runMode: 1,
+      // Configure retry attempts for `cypress open`
+      // Default is 0
+      openMode: 1,
+    },
+    video:true,
+    videoCompression:32,
+    screenshotOnRunFailure: true,
     setupNodeEvents(on, config) {
+      allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
       // implement node event listeners here
+      
     },
   },
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import 'allure-cypress';
 
 
 const app = window.top;
